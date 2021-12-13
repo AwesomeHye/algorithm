b9b0ff93ea7c3f29039a8e0a640968615dfc870a
hyein <catsarah3333@gmail.com>
2021-12-13 13:02:13 +0900

    문제 추가

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import sys
+
+'''
+https://www.acmicpc.net/problem/10818
+최소, 최대
+'''
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    num_list = list(map(int, sys.stdin.readline().split()))
+
+    print(min(num_list), end=' ')
+    print(max(num_list))
+
+
